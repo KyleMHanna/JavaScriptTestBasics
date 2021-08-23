3a280f811bbf4dae5041c26bb81b9126c7a01f2b
Kyle Hanna <ikyle.hanna@gmail.com>
2021-08-23 14:19:59 -0600

    grade possible

--- app/conditionals.js
+++ app/conditionals.js
@@ -14,6 +14,17 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
+let grade = (score/possible)*100
+if (grade>= 90)
+return "A"
+else if (grade>= 80)
+return "B"
+else if (grade>= 70)
+return "C"
+else if (grade>= 60)
+return "D"
+else if (grade>= 50)
+return "F"
 
 }
 
